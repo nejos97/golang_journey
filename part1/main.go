bf845b1e46e84c2a646ff02c0a51f9ed0c375494
Jonathan Nenba <nenbajonathan@protonmail.com>
2023-06-03 23:31:30 +0100

    create function and struct

--- part1/main.go
+++ part1/main.go
@@ -6,4 +6,35 @@ import "fmt"
 
 func main() {
 	fmt.Println("Condition in Golang")
+
+	if height := 5; height > 6 {
+		fmt.Println("You are tall.")
+	} else if height > 4 {
+		fmt.Println("You are still tall but not much.")
+	} else {
+		fmt.Println("You are not tall at all.")
+	}
+	var myCar car = car{
+		Make:   "Toyota",
+		Model:  "Yaris",
+		Height: 20,
+		Width:  30,
+	}
+	fmt.Println(myCar.Make)
+
+}
+
+func sub(s1 string, s2 string) string {
+	return s1 + s2
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+type car struct {
+	Make   string
+	Model  string
+	Height int
+	Width  int
 }
